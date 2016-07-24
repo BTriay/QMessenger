63922418d3613d0a40403f7479f58a154de51ff9
Benoit <benoit.triay+13@gmail.com>
2016-07-24 21:08:08 +0100

    Move code from main to UI classes

--- client/QMessenger/main.cpp
+++ client/QMessenger/main.cpp
@@ -1,38 +1,17 @@
 #include <QApplication>
 #include <iterator>
 #include <QMessageBox>
+#include <string>
 
-#include "../../parser.h"
-#include "StartWindow.h"
+#include "MainWindow.h"
+#include "RoomWindow.h"
 
 int main(int argc, char* argv[]) {
 
     QApplication app(argc, argv);
 
-    std::string filename = "profile";
-    std::vector<std::string> tokens;
-    std::vector<std::string>::iterator it;
-    QString username;
-    QString server;
-
-    if (configFileReader(filename, tokens)) {
-        for (it = tokens.begin(); it != tokens.end(); it++) {
-            if (*it == "username") {
-                it++;
-                username = QString::fromStdString(*it);
-            }
-            else if (*it == "domain") {
-                it++;
-                server = QString::fromStdString(*it);
-            }
-        }
-    }
-    else {
-        QMessageBox::warning(NULL, "QMessenger", "There is no config file");
-    }
-
-    StartWindow w(username, server);
-    w.show();
+    MainWindow m;
+    m.show();
 
     return app.exec();
 }
