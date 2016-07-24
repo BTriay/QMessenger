31772fd23a04178dbb3d854d18bed6e391e0b693
Benoit <benoit.triay+13@gmail.com>
2016-07-24 21:10:55 +0100

    Connect to thr, login / register a new user

---
+++ client/QMessenger/ConnectionWindow.cpp
@@ -0,0 +1,120 @@
+#include "ConnectionWindow.h"
+
+/* ************************************ PUBLIC FUNCTIONS ************************************ */
+ConnectionWindow::ConnectionWindow(QTcpSocket* socket) : QWidget () {
+qDebug() << "connection window constructor";
+    std::string username;
+    std::string domain;
+    std::string port;
+    readProfile(username, domain, port);
+
+    setWindowTitle("QMessenger - Login");
+
+    a_lblUsername = new QLabel("&Username");
+    a_lblServer = new QLabel("&Server");
+    a_lblPort = new QLabel("&Port");
+    a_lblPwd =  new QLabel("Pass&word");
+    a_lblPwdConf =  new QLabel("Con&firm password");
+    a_leUsername = new QLineEdit(QString(username.c_str()));
+    a_lblUsername->setBuddy(a_leUsername);
+    a_leServer = new QLineEdit(QString(domain.c_str()));
+    a_lblServer->setBuddy(a_leServer);
+    a_lePort = new QLineEdit(QString(port.c_str()));
+    a_lblPort->setBuddy(a_lePort);
+    a_lePwd = new QLineEdit();
+    a_lblPwd->setBuddy(a_lePwd);
+    a_lePwdConf = new QLineEdit("<color=grey>[new user only]</color=grey>");
+    a_lblPwdConf->setBuddy(a_lePwdConf);
+    a_pbNewUser = new QPushButton("&New user");
+    a_pbConnect = new QPushButton("&Connect");
+    a_pbExit = new QPushButton("E&xit");
+    a_leMessage = new QLabel();
+
+    a_layoutMain = new QGridLayout();
+    a_layoutMain->addWidget(a_lblUsername,0,0);
+    a_layoutMain->addWidget(a_lblServer,1,0);
+    a_layoutMain->addWidget(a_lblPort,2,0);
+    a_layoutMain->addWidget(a_lblPwd,3,0);
+    a_layoutMain->addWidget(a_lblPwdConf,4,0);
+    a_layoutMain->addWidget(a_leUsername,0,1);
+    a_layoutMain->addWidget(a_leServer,1,1);
+    a_layoutMain->addWidget(a_lePort,2,1);
+    a_layoutMain->addWidget(a_lePwd,3,1);
+    a_layoutMain->addWidget(a_lePwdConf,4,1);
+    a_layoutMain->addWidget(a_pbConnect,0,2);
+    a_layoutMain->addWidget(a_pbNewUser,1,2);
+    a_layoutMain->addWidget(a_pbExit,2,2);
+    a_layoutMain->addWidget(a_leMessage,5,0,1,3);
+
+    a_layoutMain->setAlignment(Qt::AlignCenter);
+
+    this->setLayout(a_layoutMain);
+    a_socket = socket;
+
+    QObject::connect(a_pbExit, SIGNAL(clicked()), this, SLOT(close()));
+    QObject::connect(a_pbConnect, SIGNAL(clicked()), this, SLOT(knownUser()));
+    QObject::connect(a_pbNewUser, SIGNAL(clicked()), this, SLOT(newUser()));
+    QObject::connect(a_socket, SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(socketErr(QAbstractSocket::SocketError)));
+    QObject::connect(a_socket, SIGNAL(connected()), this, SLOT(serverConnectionSuccess()));
+}
+
+/* ************************************ END OF PUBLIC FUNCTIONS ************************************ */
+
+/* ************************************ PRIVATE FUNCTIONS ************************************ */
+void ConnectionWindow::readProfile(std::string& username, std::string& domain, std::string& port) {
+    std::string filename = "profile";
+    std::vector<std::string> tokens;
+    std::vector<std::string>::iterator it;
+
+    if (configFileReader(filename, tokens)) {
+        for (it = tokens.begin(); it != tokens.end(); it++) {
+            if (*it == "username") {
+                it++;
+                username = *it;
+            }
+            else if (*it == "domain") {
+                it++;
+                domain = *it;
+            }
+            else if (*it == "port") {
+                it++;
+                port = *it;
+            }
+        }
+    }
+}
+
+void ConnectionWindow::serverConnectionAttempt() {
+    a_socket->abort();
+    bool ok;
+    quint16 port = this->a_lePort->text().toUShort(&ok);
+    if (ok) {
+        a_leMessage->setText("Connecting to the server");
+        a_socket->connectToHost(this->a_leServer->text(), port);
+    }
+    else
+        a_leMessage->setText("Invalid port number");
+}
+/* ************************************ END OF PRIVATE FUNCTIONS ************************************ */
+
+/* ************************************ SLOTS ************************************ */
+void ConnectionWindow::serverConnectionSuccess() {
+    a_leMessage->setText("Connected");
+}
+
+void ConnectionWindow::socketErr(QAbstractSocket::SocketError err) {
+    QString errMsg;
+    errMsg = a_socket->errorString();
+    a_leMessage->setText(errMsg);
+}
+
+void ConnectionWindow::newUser() {
+    serverConnectionAttempt();
+
+}
+
+void ConnectionWindow::knownUser() {
+    serverConnectionAttempt();
+
+}
+/* ************************************ END OF SLOTS ************************************ */
---
+++ client/QMessenger/ConnectionWindow.h
@@ -0,0 +1,57 @@
+#ifndef CONNECTIONWINDOW_H
+#define CONNECTIONWINDOW_H
+
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QLabel>
+#include <QGridLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+
+#include <QString>
+#include <QTcpSocket>
+#include <arpa/inet.h>
+
+#include "../../parser.h"
+
+class ConnectionWindow : public QWidget {
+Q_OBJECT
+
+private:
+    QGridLayout* a_layoutMain;
+    QLabel* a_lblUsername;
+    QLabel* a_lblServer;
+    QLabel* a_lblPort;
+    QLabel* a_lblPwd;
+    QLabel* a_lblPwdConf;
+    QLineEdit* a_leUsername;
+    QLineEdit* a_leServer;
+    QLineEdit* a_lePort;
+    QLineEdit* a_lePwd;
+    QLineEdit* a_lePwdConf;
+    QLabel* a_leMessage;
+    QPushButton* a_pbConnect;
+    QPushButton* a_pbExit;
+    QPushButton* a_pbNewUser;
+
+    void readProfile(std::string& username, std::string& domain, std::string& port);
+    void serverConnectionAttempt();
+
+    QTcpSocket* a_socket;
+
+public:
+    ConnectionWindow(QTcpSocket* socket);
+
+signals:
+
+public slots:
+    void serverConnectionSuccess();
+    void socketErr(QAbstractSocket::SocketError err);
+    void newUser();
+    void knownUser();
+};
+
+#endif // CONNECTIONWINDOW_H
