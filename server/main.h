92e450372628ceb7c849c394c8e3c307f287974d
Benoit <benoit.triay+13@gmail.com>
2016-07-26 23:41:31 +0100

    Move struct bundle declaration to dedicated header, and threads functions declarations to ThreadFunc.h

--- server/main.h
+++ server/main.h
@@ -8,21 +8,10 @@
 #include "connection.h"
 #include "Db.h"
 #include "MatrixServer.h"
+#include "Bundle.h"
 
-typedef struct Bundle Bundle;
-
-struct Bundle {
-	MatrixServer* matrix;
-	Db* db;
-	int epfd; //for init only, not used in the threads
-	int threadNo; //for init only, not used in the threads
-	std::string portNo; //for init only, not used in the threads
-};
-
+//main.cpp
 void bundleInit(Bundle& mydata);
 void bundleClean(Bundle& serverData);
 void threadsLaunch(Bundle& serverData);
 void* threadStart(void *arg);
-
-void getSocketMsg(std::string& msg, int fd);
-void rearmFD(int epfd, int fd);
