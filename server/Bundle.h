a0accd6f92395240ef6bddf17903880af8201937
Benoit <benoit.triay+13@gmail.com>
2016-07-26 23:37:51 +0100

    Move struct definition to header as it is reused in different cpp files

---
+++ server/Bundle.h
@@ -0,0 +1,9 @@
+typedef struct Bundle Bundle;
+
+struct Bundle {
+	MatrixServer* matrix;
+	Db* db;
+	int epfd;
+	int threadNo; //for init only, not used in the threads
+	std::string portNo; //for init only, not used in the threads
+};
